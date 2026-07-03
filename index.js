5e15aa48b8acc26e5e1fc677f4a0bddc1d189312
nchinthan <146515522+nchinthan@users.noreply.github.com>
2026-07-03 21:27:11 +0530

    .

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +15,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
